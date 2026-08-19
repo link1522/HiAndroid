09e9b2746fd905be8a431d6775f2f97a2dff3f47
Link1515 <terrylin1515@gmail.com>
2026-08-19 15:45:00 +0800

    chore: remove unused greeting composable and preview

--- app/src/main/java/com/example/hiandroid/MainActivity.kt
+++ app/src/main/java/com/example/hiandroid/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
@@ -26,22 +25,6 @@ class MainActivity : ComponentActivity() {
                 AppNavigation()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    HiAndroidTheme {
-        Greeting("Android")
     }
 }
 
