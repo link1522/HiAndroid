f6a4ffc4b7161941bd7e6b21bb6d3dd8c2b625d1
Link1515 <terrylin1515@gmail.com>
2026-08-19 15:24:52 +0800

    feat: add compose navigation between home and about

--- app/src/main/java/com/example/hiandroid/MainActivity.kt
+++ app/src/main/java/com/example/hiandroid/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.hiandroid.ui.theme.HiAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HiAndroidTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNavigation()
             }
         }
     }
@@ -43,5 +42,48 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     HiAndroidTheme {
         Greeting("Android")
+    }
+}
+
+@Composable
+fun HomeScreen(onAboutClick: () -> Unit ) {
+    Column(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+        Text(text = "首頁")
+
+        Button(onClick = onAboutClick) {
+            Text(text = "前往 About")
+        }
+    }
+}
+
+@Composable
+fun AboutScreen(onHomeClick: () -> Unit ) {
+    Column(modifier = Modifier.fillMaxSize().safeDrawingPadding()) {
+        Text(text = "About")
+
+        Text (text = "my app")
+
+        Button(onClick = onHomeClick) {
+            Text(text = "回首頁")
+        }
+    }
+}
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+        composable ("home") {
+            HomeScreen(onAboutClick = {
+                navController.navigate("about")
+            })
+        }
+
+        composable("about") {
+            AboutScreen(onHomeClick = {
+                navController.navigate("home")
+            })
+        }
     }
 }
